6e4f4719e831860d1cd7565f100d244c90b24748
Oğuzhan Yukacı <aoguzhanyukaci@gmail.com>
2021-04-02 15:49:59 +0300

    favorite movies

--- MovieNight/Controllers/HomeController.cs
+++ MovieNight/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using MovieNight.Models;
+using MovieNight.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,12 +8,35 @@ using System.Web.Mvc;
 
 namespace MovieNight.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
-        ApplicationDbContext db = new ApplicationDbContext();
-        public ActionResult Index()
+        public ActionResult Index(int page = 1)
         {
-            return View(db.Movies.ToList());
+            IQueryable<Movie> query = db.Movies;
+            int totalItems = query.Count();
+            int pageSize = 9;
+            int totalPages = (int)Math.Ceiling(totalItems / (decimal)pageSize);
+            List<Movie> movies = query
+                .OrderByDescending(x => x.ImdbRating)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            var vm = new HomeViewModel()
+            {
+                Movies = movies,
+                PaginationInfo = new PaginationInfoViewModel()
+                {
+                    CurrentPage = page,
+                    PageSize = pageSize,
+                    ItemsOnPage = movies.Count,
+                    TotalPages = totalPages,
+                    HasNext = page < totalPages,
+                    HasPrevious = page > 1
+                }
+            };
+
+            return View(vm);
         }
 
         public ActionResult About()
@@ -29,13 +53,6 @@ namespace MovieNight.Controllers
             return View();
         }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);   
-        }
+
     }
 }
